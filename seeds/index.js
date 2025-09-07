9efa44534a8ecc6562374fc7f5c0437d7c2c4104
Fauzan Aditya Harsya <fauzan.harsya@gmail.com>
2025-09-07 21:06:28 +0700

    edit seeds, fix css image inconsistency

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const axios = require('axios');
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const Campground = require('../models/campground');
 const { places, descriptors } = require('./seedHelpers');
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const { cloudinary } = require("../cloudinary")
+
+const dbUrl = process.env.DB_URL;
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -11,19 +16,52 @@ db.once("open", () => {
   console.log("Database connected");
 })
 
+async function getRandomImage() {
+  try {
+    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: process.env.UNSPLASH_ACCESS_KEY,
+        collections: 1114848,
+      },
+    });
+    return resp.data.urls.regular;
+  } catch (err) {
+    console.error("Error fetching from Unsplash:", err.message);
+  }
+}
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 25; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
+
+    const images = [];
+    for (let j = 0; j < 2; j++) {
+      const imageUrl = await getRandomImage();
+      if (imageUrl) {
+        try {
+          const uploadedResponse = await cloudinary.uploader.upload(
+            imageUrl,
+            { folder: 'YelpCamp' }
+          )
+          images.push({
+            url: uploadedResponse.secure_url,
+            filename: uploadedResponse.public_id
+          })
+        } catch (uploadErr) {
+          console.log("Error uploading to cloudinary: ", uploadErr.message);
+        }
+      }
+    }
+
     const camp = new Campground({
-      author: '68ace7bb8dcf8b3fa2af95a6',
+      author: '68bd765ce0ed4f3ff5400aa7',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, dignissimos sint. Tempora fuga ab impedit voluptatibus dolore cupiditate, adipisci at perferendis repellendus dicta! Quidem magni iure minus similique quod architecto?',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit atque earum dicta excepturi aut sint id incidunt ratione totam dolorem ullam nostrum, omnis ut officiis eveniet explicabo iure laudantium. Aliquid?',
       price,
       geometry: {
         type: 'Point',
@@ -32,22 +70,11 @@ const seedDB = async () => {
           cities[random1000].latitude
         ]
       },
-      images: [
-        {
-          url: 'https://res.cloudinary.com/dej7fxqqa/image/upload/v1756540696/YelpCamp/hsvfhdq6rd2fr9o2j7gy.avif',
-          filename: 'YelpCamp/hsvfhdq6rd2fr9o2j7gy'
-        },
-        {
-          url: 'https://res.cloudinary.com/dej7fxqqa/image/upload/v1756540694/YelpCamp/fnxiint4upu3jowss3dp.jpg',
-          filename: 'YelpCamp/fnxiint4upu3jowss3dp'
-        }
-      ],
+      images: images,
     })
     await camp.save();
   }
 }
-
-
 
 seedDB()
   .then(() => {
@@ -60,4 +87,3 @@ seedDB()
     console.log(err);
     mongoose.connection.close();
   })
-
